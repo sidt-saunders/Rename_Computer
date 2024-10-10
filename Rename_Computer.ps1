308183c63ac2b9ca7f6e92353cc6b9aebe8d1231
Sidt Saunders <sidt.saunders+github@gmail.com>
2024-10-10 00:55:26 -0400

    Done

--- Rename_Computer.ps1
+++ Rename_Computer.ps1
@@ -1,34 +1,100 @@
-﻿#Get the current computer name
-$CurrentDeviceHostname = $env:COMPUTERNAME
+﻿#----------
+#Variables
+#----------
 
-#Prompt the user to enter the 4 character code
-$CharCode = Read-Host -Prompt "`nEnter the 4 character code"
+$FailCounter = 0
 
-#Validate the input
-If ($CharCode.Length -ne 4) {
-    Write-Host "`nInvalid input. The code should be exactly 4 characters long." -ForegroundColor DarkRed
+#----------
+#Functions
+#----------
+
+Function NotValidCharlength() {
+    While ($FailCounter -ne 2) {
+        Write-Host ""
+        Write-Warning "Code should be exactly 4 characters long. "
+        $FailCounter++
+        CharCodePrompt
+    }
+    Write-Host ""
+    Write-Error "Too many failed attempts. Script will exit in 5 seconds."
+    Start-Sleep -Seconds 5
+    $FailCounter = 0
     Exit
 }
 
-#Generate the new hostname
-$NewDeviceHostname = $CharCode + $CurrentDeviceHostname.Substring(4)
+Function NotValidCharType() {
+    While ($FailCounter -ne 2) {
+        Write-Host ""
+        Write-Warning "Code should be characters only. "
+        $FailCounter++
+        CharCodePrompt
+    }
+    Write-Host ""
+    Write-Error "Too many failed attempts. Script will exit in 5 seconds."
+    Start-Sleep -Seconds 5
+    $FailCounter = 0
+    Exit
+}
 
-#Rename the computer
-Rename-Computer -NewName $NewDeviceHostname -Force
+Function CharCodePrompt() {
+    $CharCode = Read-Host -Prompt "Enter the 4 character code (USAL, USHO, USGR, etc.)"
+    #Validate input
+    If ($CharCode.Length -ne 4) {NotValidCharlength}
+    If ($CharCode -notmatch "[a-zA-Z]") {NotValidCharType}
+    Else {GenerateAndRename}
+}
 
-#Create Function
+Function GenerateAndRename() {
+    #Generate new hostname
+    $NewDeviceHostname = $CharCode.ToUpper() + $CurrentDeviceHostname.Substring(4)
+    #Rename device
+    Rename-Computer -NewName $NewDeviceHostname -Force
+    PromptToRestart
+}
+
+Function PromptToRestart() {
+    $DeviceRestart = Read-Host -Prompt "`nDevice renamed successfully. Do you want to restart now? (Y/N) "
+
+    Switch ($DeviceRestart) {
+        Y {TimeToRestart}
+        N {
+            Write-Host "`nPlease restart as soon as possible for the name change to propagate.`nScript will exit in 5 seconds.`n"
+            Start-Sleep -Seconds 5
+            Exit
+        }
+
+        Default {NotValidRestart}
+    }
+}
+
 Function TimeToRestart() {
     Write-Host "`nDevice will restart in 5 seconds." -ForegroundColor DarkGreen
     Start-Sleep -Seconds 5
     Restart-Computer -Force
 }
 
-#Prompt for a restart
-$DeviceRestart = Read-Host -Prompt "`nDevice renamed successfully. Do you want to restart now? (Y/N)"
-
-Switch ($DeviceRestart) {
-    Y {TimeToRestart}
-    N {Write-Host "`nPlease restart your device for the name change to propagate`n." -ForegroundColor DarkCyan}
-
-    Default {Write-Host "`nPlease enter a valid response" -ForegroundColor DarkRed}
+Function NotValidRestart() {
+    While ($FailCounter -ne 2) {
+        Write-Host ""
+        Write-Warning "Please enter a valid response. "
+        $FailCounter++
+        PromptToRestart
+    }
+    Write-Host ""
+    Write-Error "Too many failed attempts. Script will exit in 5 seconds."
+    Start-Sleep -Seconds 5
+    $FailCounter = 0
+    Exit
 }
+
+#----------
+#Script
+#----------
+
+#Get the current computer name
+$CurrentDeviceHostname = $Env:COMPUTERNAME
+
+Write-Host "`nThis script will allow you to update the 4 letter character in the hostname.`n" -BackgroundColor Black -ForegroundColor White
+Write-Host "NOTE: Device needs to restart for changes to take effect.`nPress CTRL +C at any time to interrupt the script.`n`n------------------------------`n`n"
+
+CharCodePrompt
